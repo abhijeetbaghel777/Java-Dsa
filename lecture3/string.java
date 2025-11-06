e7846a35ec1d9fea50a64571a2075503e3496d9c
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-11-06 10:10:38 +0530

    string methods

---
+++ lecture3/string.java
@@ -0,0 +1,14 @@
+// string methods
+public class string {
+    public static void main(String[] args) {
+        String str="hello Java";
+        System.out.println(str);
+        System.out.println(str.substring(1));
+        System.out.println(str.charAt(2));
+       System.out.println(str.indexOf("J"));
+       System.out.println(str.trim());
+
+    }
+
+    
+}
