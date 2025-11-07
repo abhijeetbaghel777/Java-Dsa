e75a858e79bbf57aa2f4284166620c4dac83b401
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-11-07 13:41:18 +0530

    bigest

--- lecture4/anagram.java
+++ lecture4/anagram.java
@@ -1,4 +1,22 @@
+import java.util.Arrays;
+
 public class anagram {
-      
+      public static void main(String[] args) {
+        String str="hello";
+        String str1="hlloe";
+        char ch[]=str.toCharArray();
+        char ch1[]=str1.toCharArray();
+        Arrays.sort(ch);
+        String val1=new String(ch);
+        String val2=new String(ch1);
+        if(val1.equals(val2)){
+            System.out.println("true");
+        }
+        else System.out.println("false");
+
+
+        
+
+      }
 }
 
