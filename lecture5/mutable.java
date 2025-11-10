0b7e15c306ab3e03a384d86517527247dde20d02
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-11-10 12:56:20 +0530

    stringbuilder

---
+++ lecture5/mutable.java
@@ -0,0 +1,28 @@
+public class mutable {
+    public static void main(String[] args) {
+        StringBuilder str= new StringBuilder("programmming");
+    //     str.insert(2,"hii");//insert at position
+    //     System.out.println(str);
+    //     str.append(" wow");//add a char at last
+    //     System.out.println(str);
+    // replace()
+    // str.replace(1,4,"java");
+    // System.out.println(str);
+    // 4.reversal
+    // str.reverse();
+    // System.out.println(str);
+    // 5.delete()
+    // str.delete(1,3);// end index wala character delete  nahi hota h
+    // System.out.println(str);
+    //6.deletechatat()
+    // str.deleteCharAt(0);
+    // System.out.println(str);
+    //7.setcharat();
+    str.setCharAt(1,'x');
+    System.out.println(str);
+    }
+    
+}
+
+    
+
---
+++ lecture5/stringbuilder.java
@@ -0,0 +1,8 @@
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder str=new StringBuilder("HELLO");// use create a new string ie mutable and creates new object each time
+        System.out.println(str);
+    }
+
+   
+}
