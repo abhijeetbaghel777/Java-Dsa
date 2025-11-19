dbde2b985997773e679fb2025f288b77244b3201
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-11-19 12:58:33 +0530

    m

--- lecture8/methodoverriding.java
+++ lecture8/methodoverriding.java
@@ -8,11 +8,15 @@ class dog extends animal{
     void eat(){
         System.out.println("roti");
     }
+    void parenteat(){
+        super.eat();
+    }
 }
 
 
 class methodoverriding {// can not be used without inheritence
 public static void main(String[] args) {
-    animal s1=new dog();// a child class object can be made from the parent class refrence but vice versa is not possible
+    dog s1=new dog();// a child class object can be made from the parent class refrence but vice versa is not possible
     s1.eat();
-}}
+    s1.parenteat();
+}}// agar parent class k refrence se dog class ka object banayenge toh super keyword k through parent class ka function access nahi kar payenge
