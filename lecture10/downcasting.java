92b869c6873273fbfe30c1bf85df942cf14a9e27
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-11-19 13:33:26 +0530

    down casting

---
+++ lecture10/downcasting.java
@@ -0,0 +1,31 @@
+class animal{
+
+    void eat(){
+        System.out.println("pizza");
+    } void play(){
+        System.out.println("football");
+    }
+}
+class dog extends animal{
+    void eat(){
+        
+        System.out.println("roti");
+    }
+    void play(){
+        System.out.println("volleyball");
+    }
+    void greet()
+    {
+        System.out.println("bark");
+    }
+}
+
+public class downcasting {
+    public static void main(String[] args) {
+     animal s1=new dog();
+     dog s2=(dog)s1;// down casting
+     s2.greet();//with this now the methods that are not overrideen can also be accessed
+     
+
+    }
+}
