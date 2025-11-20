39dbc1f19a92e92ba5edd9a8b345528a38ae5a66
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-11-20 13:32:00 +0530

    why not multiple inheritence

---
+++ lecture11/multipleInheritence.java
@@ -0,0 +1,16 @@
+// multiple inherutence is not supported in java
+/*mutiple inhertence= a single child class having mutiple parent class
+why it is not supported in java? */
+// class a{
+// void sleep(){
+// System.out.println("hii");
+// }
+// }
+// class b{
+// void sleeep(){
+// System.out.println("helo");
+// }
+// }
+// class c extends a,b{// in this case when the void function will be called then there will ne a conflict on which method to use
+
+// }
