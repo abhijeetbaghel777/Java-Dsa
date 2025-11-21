383e57ce1c216f8a980e4828f5fd5204d502ce29
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-11-21 14:21:26 +0530

    randon4 umber

---
+++ lecture12/q1.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+class fan {
+    private int fanspeed=5;
+    public void  getspeed(){
+        System.out.println("Speed of fan is "+fanspeed);
+    }
+    public void setlow(int x){
+        
+           
+}
+}
+class q1{
+    public static void main(String[] args) {
+        Random r1=new Random();
+        int n=r1.nextInt(100000,1000000);
+        System.out.println("OTP= "+n);
+    }
+}
