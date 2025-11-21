15e18db52cb80994dfa8049e08836ba949213c72
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-11-21 15:25:46 +0530

    encapsulation practise

--- lecture12/q1.java
+++ lecture12/q1.java
@@ -1,19 +1,24 @@
-import java.util.Random;
+// import java.util.Random;
 
 class fan {
     private int fanspeed=5;
     public void  getspeed(){
         System.out.println("Speed of fan is "+fanspeed);
     }
-    public void setlow(int x){
-        
+    public void setspeed(int x){
+    if(x>fanspeed)
+        System.out.println("speed can't be greater then max speed");    
+    else if(x==0)
+        System.out.println("the fan stoppped...");
+    else if(x<0)
+        System.out.println("speed can't be less then zero");
+    else
+        System.out.println("the speed is now "+x);
            
 }
 }
 class q1{
     public static void main(String[] args) {
-        Random r1=new Random();
-        int n=r1.nextInt(100000,1000000);
-        System.out.println("OTP= "+n);
-    }
+       fan f1=new fan();
+    f1.setspeed(-1);   }
 }
