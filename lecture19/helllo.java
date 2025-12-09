99e30d9e7e5382e355034fe3cf45834530116d77
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-12-09 15:45:10 +0530

    throwing exceptio

--- lecture19/helllo.java
+++ lecture19/helllo.java
@@ -1,5 +1,22 @@
+// throwing exceptiuon manually
 public class helllo {
     public static void main(String[] args) {
-        
+        try{
+            voteage(12);
+        }catch (Exception e){
+          System.out.println(e);
+        }
+        try{
+            voteage(19);
+        }catch(Exception e){
+System.out.println(e);
+        }
+    }
+     static void voteage(int age){
+        System.out.println("check for vote");
+        if(age<18){
+            throw new IllegalArgumentException ("nahi de sakte vote");
+        }else
+            System.out.println("dede vote bhai");
     }
 }
