85a6e644b06a8dc3ed3148fd100be600dd8c6fcf
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-12-10 15:29:02 +0530

    codes

---
+++ lecture19/question.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class question {
+    public static void main(String[] args) {
+        int[] arr={5,-3,0,6,3,7};
+        int k=3;
+        // to get  a pair of maximum sub array
+        ArrayList<Integer> list =new ArrayList<>();
+        for(int x:arr){
+          list.add(x);
+
+        }
+        for(int y:list){
+            
+        }
+    }
+}
