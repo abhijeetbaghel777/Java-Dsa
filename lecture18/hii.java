ccd003a87f62670f512523231ffe45ff511b7257
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-12-11 10:32:21 +0530

    iterator basics

---
+++ lecture18/hii.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class hii{
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        int[] arr={20,10,12,14,3,10};
+        for(int x:arr){
+            list.add(x);
+        }
+        Iterator<Integer> it=list.listIterator();
+        int target=14;
+        while(it.hasNext()){
+         int z=it.next();
+         if(z==target){
+            break;
+         }
+         if()
+        }
+    }
+}
+            
