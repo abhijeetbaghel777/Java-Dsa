aefc79fa32f55176302468f54490b6dca78b1297
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2025-12-17 15:36:26 +0530

    codes

--- lecture20/fh1.java
+++ lecture20/fh1.java
@@ -1,17 +1,28 @@
-import java.io.File;
+
+import java.io.FileReader;
 import java.io.FileWriter;
 
 public class fh1 {
     public static void main(String[] args) {
-       
+       createData();
     }
-    static void createData(String str){
+    static void createData(){
         try {
-          FileWriter writer=new FileWriter(str)  ;
-          writer.write("heloo mera naya data");
+          FileWriter writer=new FileWriter("index.html",true)  ;// file data inserter
+          writer.write(" mera naya data");
           writer.close();
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+    static void readData(){
+        try {
+            FileReader reader=new FileReader("index.html");
+            int ch;
+            while(ch=reader.read()!=-1){
+                System.out.print((char)ch);
+            }
+        } catch (Exception e) {
+            
     }
 }
---
+++ lecture23/lcCode1.java
@@ -0,0 +1,9 @@
+/*dutch national flag= used for three unique values
+algorithm->
+1. take three pointer 
+int l=0;
+it m=0;
+int h=n-1;
+certain rules to follow->
+ index will be moving from following pattern
+ */
